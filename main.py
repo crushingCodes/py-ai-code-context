2166bc3f6203ee0b5c2532000eb6ffaafac18739
Gavin <gavinjboyd@gmail.com>
2023-04-17 03:29:03 +0900

    basic chatbot working for project code

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import os
 from pathlib import Path
 
 from git.repo.base import Repo
-from llama_index import GPTSimpleVectorIndex, ServiceContext, download_loader
+from langchain import OpenAI
+from llama_index import (GPTSimpleVectorIndex, LLMPredictor, PromptHelper,
+                         ServiceContext, download_loader)
+from llama_index.node_parser import SimpleNodeParser
 
 from src.context_loader import generate_files
 
@@ -14,8 +17,10 @@ TEMP_PATH = Path(os.environ.get("TEMP_PATH", Path(__file__).parent.joinpath("tmp
 UnstructuredReader = download_loader("UnstructuredReader", refresh_cache=True)
 loader = UnstructuredReader()
 
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", None)
 
-def prepare_temp_file(file, doc_set, all_docs, index_set, service_context):
+
+def prepare_temp_file(file):
     temp_file = TEMP_PATH.joinpath(f"{file.name} .txt")
     file_content = ""
     print("Processing file", file.name)
@@ -26,35 +31,49 @@ def prepare_temp_file(file, doc_set, all_docs, index_set, service_context):
     return temp_file
 
 
-def ingest_data(
-    doc_set: dict,
-    all_docs: list,
-    index_set: dict,
-    service_context,
-    temp_file,
-    filename_info: str,
-):
+def extract_documents(filename_info, temp_file):
     file_docs = loader.load_data(file=temp_file, split_documents=False)
     for file_doc in file_docs:
         file_doc.extra_info = {"filename": filename_info}
-    doc_set[filename_info] = file_docs
-    all_docs.extend(file_docs)
+    return file_docs
 
-    cur_index = GPTSimpleVectorIndex.from_documents(
-        doc_set[filename_info], service_context=service_context
+
+def create_llm_index(all_docs):
+    parser = SimpleNodeParser()
+    nodes = parser.get_nodes_from_documents(all_docs)
+    llm_predictor = LLMPredictor(
+        llm=OpenAI(
+            temperature=0, model_name="text-davinci-003", openai_api_key=OPENAI_API_KEY
+        )
     )
-    # remove slashes from filename_info
-    filename_info_index = filename_info.replace("/", "_")
-    index_name = f"index_{filename_info_index}.json"
+    max_input_size = 4096
+    max_output = 256
+    max_chunk_overlap = 20
+    prompt_helper = PromptHelper(max_input_size, max_output, max_chunk_overlap)
+
+    service_context = ServiceContext.from_defaults(
+        llm_predictor=llm_predictor, prompt_helper=prompt_helper
+    )
+    cur_index = GPTSimpleVectorIndex(nodes, service_context=service_context)
+    # TODO: save index as repo name
+    index_name = "index_test.json"
     cur_index.save_to_disk(str(TEMP_PATH.joinpath(index_name)))
-    index_set[filename_info] = cur_index
+
+
+def chat():
+    index_name = "index_test.json"
+    index = GPTSimpleVectorIndex.load_from_disk(str(TEMP_PATH.joinpath(index_name)))
+    print("Chatting")
+    while True:
+        query = input("Query: ")
+        if query == "exit":
+            break
+        response = index.query(query)
+        print("Response:", response)
 
 
 def main():
-    doc_set = {}
     all_docs = []
-    index_set = {}
-    service_context = ServiceContext.from_defaults(chunk_size_limit=512)
     repo = Repo(PROJECT_ROOT)
     files = generate_files(PROJECT_ROOT, repo)
     if not TEMP_PATH.exists():
@@ -64,15 +83,14 @@ def main():
         processed_files_path.mkdir()
     for file in files:
         try:
-            temp_file = prepare_temp_file(
-                file, doc_set, all_docs, index_set, service_context
-            )
+            temp_file = prepare_temp_file(file)
             filename_info = str(file.relative_to(PROJECT_ROOT))
-            ingest_data(
-                doc_set, all_docs, index_set, service_context, temp_file, filename_info
-            )
+            all_docs = all_docs + extract_documents(filename_info, temp_file)
         except Exception as e:
             print("error", e)
+
+    create_llm_index(all_docs)
+    chat()
 
 
 if __name__ == "__main__":
